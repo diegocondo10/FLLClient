a56233d5d6e05c279dc29c36698c2187b8de9b79
Gustavo <johnnygar98@hotmail.com>
2019-10-27 22:37:12 -0500

    Estilo al filtro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './components/templates/home/home.component';
 import {PropiedadInfoComponent} from './components/propiedad/propiedad-info/propiedad-info.component';
+import { PropiedadFilterComponent } from './components/propiedad/propiedad-filter/propiedad-filter.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,11 @@ const routes: Routes = [
     path: '',
     component: HomeComponent
   },
+  {
+    path: 'buscar',
+    component: PropiedadFilterComponent
+  },
+  
   {
     path: 'propiedad/:id',
     component: PropiedadInfoComponent
