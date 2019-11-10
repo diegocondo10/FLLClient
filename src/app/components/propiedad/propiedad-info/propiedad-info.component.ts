034e5343a05a7032d28629ce38e86dc756438ed5
MrRainx <diegocondo10@yahoo.es>
2019-11-10 18:34:47 -0500

    solo code style

--- src/app/components/propiedad/propiedad-info/propiedad-info.component.ts
+++ src/app/components/propiedad/propiedad-info/propiedad-info.component.ts
@@ -4,36 +4,36 @@ import {Router, ActivatedRoute} from '@angular/router';
 import {Propiedad} from '../../../models/appCore';
 
 @Component({
-  selector: 'app-propiedad-info',
-  templateUrl: './propiedad-info.component.html',
-  styleUrls: [
-    './propiedad-info.component.css',
-    '../propiedad-list/propiedad-list.component.css'
-  ]
+    selector: 'app-propiedad-info',
+    templateUrl: './propiedad-info.component.html',
+    styleUrls: [
+        './propiedad-info.component.css',
+        '../propiedad-list/propiedad-list.component.css'
+    ]
 })
 export class PropiedadInfoComponent implements OnInit {
 
-  public propiedad: Propiedad;
+    public propiedad: Propiedad;
 
 
-  constructor(
-    private srv: PropiedadesService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) {
-  }
+    constructor(
+        private srv: PropiedadesService,
+        private router: Router,
+        private route: ActivatedRoute
+    ) {
+    }
 
-  async ngOnInit() {
-    const id = this.route.snapshot.params['id'];
-    this.propiedad = await this.srv.getPropiedadById(id);
-  }
+    async ngOnInit() {
+        const id = this.route.snapshot.params['id'];
+        this.propiedad = await this.srv.getPropiedadById(id);
+    }
 
-  buscar($event) {
-    this.router.navigate(['/buscar'], {
-      queryParams: {
-        filtro: JSON.stringify($event)
-      }
-    });
-  }
+    buscar($event) {
+        this.router.navigate(['/buscar'], {
+            queryParams: {
+                filtro: JSON.stringify($event)
+            }
+        });
+    }
 
 }
