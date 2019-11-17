6081196d95d9a262859d48da95d15319bd13696d
Gustavo <johnnygar98@hotmail.com>
2019-11-17 18:54:16 -0500

    Un bot no inteligente con mensajes mas mensajes

--- src/app/components/info/chat-basico/chat-basico.component.ts
+++ src/app/components/info/chat-basico/chat-basico.component.ts
@@ -86,6 +86,10 @@ export class ChatBasicoComponent implements OnInit {
 
             this.msgSinResponder += this.msgCli;
             this.msgCli = '';
+            // Para que nuestro bot responda 
+            setTimeout(() => {
+              this.mensajeBot();
+          }, 4000);
         }
     }
 
@@ -172,11 +176,11 @@ export class ChatBasicoComponent implements OnInit {
         let fecha = new Date();
         console.log('Hora: ' + fecha.getHours());
         if (fecha.getHours() >= 18) {
-            msg = 'Buenas noches. ' + this.getMensajeRandom(this.PROMOCIONES);
+            msg = 'Buenas noches. ' + this.getMensajeRandom(this.BIENVENIDA);
         } else if (fecha.getHours() >= 12) {
-            msg = 'Buenos tardes. ' + this.getMensajeRandom(this.PROMOCIONES);
+            msg = 'Buenos tardes. ' + this.getMensajeRandom(this.BIENVENIDA);
         } else {
-            msg = 'Buenos días. ' + this.getMensajeRandom(this.PROMOCIONES);
+            msg = 'Buenos días. ' + this.getMensajeRandom(this.BIENVENIDA);
         }
 
         return msg;
@@ -197,19 +201,49 @@ export class ChatBasicoComponent implements OnInit {
         'Ofertamos un gran numero de propiedades.',
         'Si necesita mas información puede escribirnos a nuestro correo: grupoinmobiliariofll@hotmail.com',
         'Si necesita hablar con un asesor de ventas puede comunicarse a los siguientes números: telf 2804483 cel 098475173',
-        'Puede escribirnos a nuestro Whatsapp para cualquier consulta o si necesita mas información.'
+        'Puede escribirnos a nuestro Whatsapp para cualquier consulta o si necesita mas información.',
+        'Tenemos promociones interesantes ^-^ Sólo déjanos tus datos aquí',
+        'Por favor déjanos tus datos aqui y te contactaremos :) ',
+        'Genial !!.. Por favor ingresa tus datos a continuación y en breve te llamaremos',
+        'Responderemos tus dudas ^-^ Solo déjanos tus datos y en breve te llamaremos',
+        'Genial !!.. Ahora solo déjanos tus datos y en seguida te llamaremos',
+        'Un gusto en ayudarte ;) por favor ingresa tus datos y en breve te llamaremos :)',
+        'Te ayudaremos ^-^ Por favor déjanos tus datos y en seguida te llamaremos',
+        'Un gusto en ayudarte :) Déjanos tus datos y número de contacto y en breve te llamaremos..',
+        'Conozca tambien nuestras promociones y asesoramientos. Déjanos tus datos y te contactaremos',
     ];
 
     private RANDOM = [
         'Puede dejarnos su información de contacto para nosotros comunicarnos con usted.',
         'Cualquier consulta o duda puede dejarlas aquí.',
-        'Si necesita vender o comprar nosotros somos su mejor opcion.'
+        'Si necesita vender o comprar nosotros somos su mejor opcion.',
+        'Hemos recibido tu mensaje, uno de nuestros agentes te contactará lo mas pronto :)',
+        'Que chévere!! ^-^ Hemos recibido tu mensaje en breve uno de nuestros agentes se pondrá en contacto contigo.',
+        'Mensaje captado ^-^ Uno de nuestros agentes te llamará en breve. Gracias por preferirnos :)',
+        'Gracias !!..  Hemos recibido tu mensaje, en seguida uno de nuestros agentes de contactará :) ',
+        'Tu mensaje a sido enviado, nuestro equipo te contactará en breve :)',
+        'El equipo de FLL recibió tu mensaje ^-^ Uno de nuestros agentes se contactará contigo.',
+        'Grupo Inmobiliario FLL recibió tu mensaje :) Ahora un agente se pondrá en contacto contigo.',
+        'Hey, muy bien!! ^-^ Hemos recibido tu mensaje. En breve un agente inmobiliario te contactará.',
+        'Un gusto en atenderte. El equipo de FLL recibió tu mensaje, ahora un agente te contactará lo mas pronto.'
     ];
 
     private PREGUNTAS = [
         'Contamos con hermosas casas y terrenos, todas las puede encontrar en la seccion buscar.',
         'Contamos con mas de ' + this.getRandomNumber() + ' casas. ',
         'Si tenemos arriendos, para mas información sobre ellos nos puede llamar a los siguientes numeros telf 2804483 cel 098475173 o dejarnos sus datos y nosotros nos podremos en contacto con usted.'
+    ];
+
+    private BIENVENIDA = [
+      'Hola. Bienvenid@ :) ¿En qué te podemos ayudar?',
+      'Hey, hola. Un gusto en atenderte :) ¿Te interesa alguna propiedad?',
+      'Hola. Si te interesa alguna propiedad, aqui te ayudamos a conseguirla :)',
+      'Hola, sabemos que necesitas un descanso y una de nuestras Quintas es ideal para ti :) Dinos que deseas saber',
+      'Hey, hola. Gracias por visitarnos :) Puedes preguntar lo que desees aquí..',
+      'Hola ^-^ Mucho gusto. ¿En que te podemos ayudar?',
+      'Hey, hola.. llegas a tiempo, tenemos asesoramiento gratis!! Pregunta aquí..',
+      'Bienvenid@! ^-^ Gracias por visitarnos, Escribe tus dudas aqui..',
+      'Hola ^-^ Te gustaria conocer nuestras hermosas Quintas :)  Déjanos tus dudas aquí..'
     ];
 
     private getRandomNumber() {
