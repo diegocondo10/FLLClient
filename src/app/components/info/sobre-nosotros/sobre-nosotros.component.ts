a324e7edd0b30df3aaf02eb1a757ec4f9cb9780b
Gustavo <johnnygar98@hotmail.com>
2019-11-29 19:55:25 -0500

    Lo movieron de lugar haha

--- src/app/components/info/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/components/info/sobre-nosotros/sobre-nosotros.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sobre-nosotros',
   templateUrl: './sobre-nosotros.component.html',
-  styleUrls: ['./sobre-nosotros.component.css']
+  styleUrls: [
+    './sobre-nosotros.component.css',
+    '../contactanos/contactanos.component.css'
+  ]
 })
 export class SobreNosotrosComponent implements OnInit {
 
